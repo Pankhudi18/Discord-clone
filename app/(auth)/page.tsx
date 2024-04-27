f9fd76b40251282390296f25599c165f2e92e7f3
Pankhudi18 <pankhudi.a.jaiswal03@gmail.com>
2024-04-27 17:28:08 +0530

    First Commit

---
+++ app/(auth)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className = "flex flex-col">
+   <p className = "text-3xl">Hello</p>
+   <Button>click me</Button>
+   </div>
+  )
+}
+ 
