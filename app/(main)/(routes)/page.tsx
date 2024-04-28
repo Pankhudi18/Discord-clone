467fb1d084a2cd84fefeaef5ef858354159ee504
Pankhudi18 <pankhudi.a.jaiswal03@gmail.com>
2024-04-28 14:39:04 +0530

    Day 2 Authentication using clerk

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -3,8 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className = "flex flex-col">
-   <p className = "text-3xl">Hello</p>
-   <Button>click me</Button>
+      This is a protectted route
    </div>
   )
 }
