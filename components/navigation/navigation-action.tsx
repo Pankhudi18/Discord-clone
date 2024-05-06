91542d089bdea3f069478fd484c2fc3393448029
Pankhudi18 <pankhudi.a.jaiswal03@gmail.com>
2024-05-06 14:47:22 +0530

    Creatng server modal done, from + sign

--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -2,14 +2,19 @@
 
 import { Plus } from "lucide-react"
 import { ActionTooltip } from "@/components/action-tooltip"
+import { useModal } from "@/hooks/use-modal-store"
 
 export const NavigationAction = () => {
+    const { onOpen } = useModal();
+
+
     return(
             <ActionTooltip
             side="right"
             align = "center"
             label = "Add a server">
                 <button
+                onClick = {() => onOpen("createServer")}
                 className = "group"
                 >
                     <div 
